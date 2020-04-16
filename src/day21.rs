1bbd47182ef850a32aad5cc58c511f8d5fc96fb6
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-04-16 17:16:33 +0200

    Day21

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,5 @@
 use std::{
-    cmp,
+    cmp::max,
     fs::File,
     io::{BufRead, BufReader},
     iter,
@@ -10,8 +10,6 @@ use lazy_static::*;
 use regex::Regex;
 
 use itertools::Itertools;
-
-use num::Integer;
 
 pub const PARTS: [fn(); 2] = [part1, part2];
 
@@ -86,71 +84,93 @@ fn load_boss() -> (i32, i32, i32) {
     boss
 }
 
-fn _maketurn(attacker: &(i32, i32, i32), defender: &mut (i32, i32, i32)) {
-    let damage = if defender.2 > attacker.1 {
-        1
-    } else {
-        attacker.1 - defender.2
-    };
+fn do_turn(
+    (p_hp, p_dps, p_armor): &mut (i32, i32, i32),
+    (b_hp, b_dps, b_armor): &mut (i32, i32, i32),
+) -> Option<bool> {
+    *b_hp -= max(*p_dps - *b_armor, 1);
     //
-    defender.0 -= damage;
+    if *b_hp <= 0 {
+        return Some(true);
+    }
+    //
+    *p_hp -= max(*b_dps - *p_armor, 1);
+    //
+    if *p_hp <= 0 {
+        return Some(false);
+    }
+    //
+    None
 }
 
-// fn playgame(mut player: (i32, i32, i32), mut boss: (i32, i32, i32)) -> bool {
-//     let mut attacker = &mut player;
-//     let mut defender = &mut boss;
-//     //
-//     let mut turn = false;
-//     while attacker.0 > 0 && defender.0 > 0 {
-//         maketurn(attacker, defender);
-//         //
-//         let temp = attacker;
-//         attacker = defender;
-//         defender = temp;
-//         //
-//         turn = !turn;
-//     }
-//     turn
-// }
-
-fn playgame(player: (i32, i32, i32), boss: (i32, i32, i32)) -> bool {
-    let pd = Integer::div_ceil(&player.0, &cmp::max(boss.1 - player.2, 1));
-    let bd = Integer::div_ceil(&boss.0, &cmp::max(player.1 - boss.2, 1));
-    pd > bd
+fn playgame(mut player: (i32, i32, i32), mut boss: (i32, i32, i32)) -> bool {
+    let mut outcome = None;
+    //
+    while outcome.is_none() {
+        outcome = do_turn(&mut player, &mut boss);
+    }
+    //
+    outcome.unwrap()
 }
 
 fn part1() {
     let (weapons, armor, rings) = load_store();
     let boss = load_boss();
     //
-    let weap_combs = weapons.iter().map(|(_, c, d, a)| (*c, *d, *a));
-    //
-    let arm_combs = iter::once((0, 0, 0))
-        .chain(armor.iter().map(|(_, c, d, a)| (*c, *d, *a)));
-    //
-    let ring_combs = iter::once((0, 0, 0))
-        .chain(rings.iter().map(|(_, c, d, a)| (*c, *d, *a)))
-        .chain(
-            rings
-                .iter()
-                .combinations(2)
-                .map(|v| (v[0].1 + v[1].1, v[0].2 + v[1].2, v[0].3 + v[1].3)),
-        );
-    //
-    let temp = weap_combs
-        .cartesian_product(arm_combs)
-        .cartesian_product(ring_combs)
+    let ans = weapons
+        .iter()
+        .map(|(_, c, d, a)| (*c, *d, *a))
+        .cartesian_product(
+            iter::once((0, 0, 0))
+                .chain(armor.iter().map(|(_, c, d, a)| (*c, *d, *a))),
+        )
+        .cartesian_product(iter::once((0, 0, 0)).chain(
+            rings.iter().map(|(_, c, d, a)| (*c, *d, *a)).chain(
+                rings.iter().enumerate().flat_map(|(i, (_, c1, d1, a1))| {
+                    rings.iter().skip(i + 1).map(move |(_, c2, d2, a2)| {
+                        (*c1 + *c2, *d1 + *d2, *a1 + *a2)
+                    })
+                }),
+            ),
+        ))
         .map(|(((c1, d1, a1), (c2, d2, a2)), (c3, d3, a3))| {
             (c1 + c2 + c3, d1 + d2 + d3, a1 + a2 + a3)
         })
-        .filter(|&player| playgame(player, boss))
-        // .min_by(|(a, _, _), (b, _, _)| a.cmp(b))
+        .filter(|&(_, d, a)| playgame((100, d, a), boss))
         .map(|(c, _, _)| c)
         .min()
         .unwrap();
-    // .collect::<Vec<_>>();
     //
-    println!("{:?}", temp);
+    println!("{}", ans);
 }
 
-fn part2() {}
+fn part2() {
+    let (weapons, armor, rings) = load_store();
+    let boss = load_boss();
+    //
+    let ans = weapons
+        .iter()
+        .map(|(_, c, d, a)| (*c, *d, *a))
+        .cartesian_product(
+            iter::once((0, 0, 0))
+                .chain(armor.iter().map(|(_, c, d, a)| (*c, *d, *a))),
+        )
+        .cartesian_product(iter::once((0, 0, 0)).chain(
+            rings.iter().map(|(_, c, d, a)| (*c, *d, *a)).chain(
+                rings.iter().enumerate().flat_map(|(i, (_, c1, d1, a1))| {
+                    rings.iter().skip(i + 1).map(move |(_, c2, d2, a2)| {
+                        (*c1 + *c2, *d1 + *d2, *a1 + *a2)
+                    })
+                }),
+            ),
+        ))
+        .map(|(((c1, d1, a1), (c2, d2, a2)), (c3, d3, a3))| {
+            (c1 + c2 + c3, d1 + d2 + d3, a1 + a2 + a3)
+        })
+        .filter(|&(_, d, a)| !playgame((100, d, a), boss))
+        .map(|(c, _, _)| c)
+        .max()
+        .unwrap();
+    //
+    println!("{}", ans);
+}
